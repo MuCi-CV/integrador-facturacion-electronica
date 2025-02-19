fb6a7e57d1ba687b47f980daffb2ef8878957df8
Lorenzo Cabrera <lorenzo@girolabs.com>
2025-02-19 17:26:20 -0300

    Add FailedOrder model

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from core.models import FailedOrder
 
-# Register your models here.
+
+@admin.register(FailedOrder)
+class FailedOrderAdmin(admin.ModelAdmin):
+    list_display = ("order_id", "colored_status")
+    list_display_links = ("order_id", "colored_status")
+    search_fields = ("order_id",)
+    ordering = ("status", "order_id")
+    list_filter = ("status",)
+
+    def colored_status(self, obj):
+        colors = {1: "red", 2: "green"}
+        return format_html(
+            '<span style="color: {};">{}</span>',
+            colors.get(obj.status, "black"),
+            obj.get_status_display(),
+        )
+
+    colored_status.admin_order_field = "status"
+    colored_status.short_description = "Estado"
--- core/models.py
+++ core/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils.timezone import now
 
-# Create your models here.
+
+class FailedOrder(models.Model):
+
+    FAILED = 1
+    COMPLETED = 2
+    STATUS_CHOICES = (
+        (FAILED, "Fallido"),
+        (COMPLETED, "Completado"),
+    )
+
+    order_id = models.IntegerField(verbose_name="ID de la orden")
+    status = models.PositiveSmallIntegerField(
+        verbose_name="Estado", choices=STATUS_CHOICES, default=FAILED
+    )
+    created_at = models.DateTimeField(default=now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Orden fallida"
+        verbose_name_plural = "Órdenes fallidas"
+
+    def __str__(self):
+        return f"Orden {self.order_id}"
