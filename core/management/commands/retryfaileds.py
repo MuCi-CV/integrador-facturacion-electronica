aec8845475b4ec29c069c98493ecdb38f1871c6f
Lorenzo Cabrera <lorenzo@girolabs.com>
2025-03-04 19:43:07 -0300

    Add more error messages

--- core/management/commands/retryfaileds.py
+++ core/management/commands/retryfaileds.py
@@ -2,6 +2,7 @@ import requests
 from django.conf import settings
 from django.core.management.base import BaseCommand
 from core.models import FailedOrder
+from datetime import datetime
 
 
 class Command(BaseCommand):
@@ -13,18 +14,33 @@ class Command(BaseCommand):
             url = settings.BASE_URL + "/sales/"
 
             for order in failed_orders:
-                response = requests.post(url, json={"arg": order.order_id}, verify=True)
+                try:
+                    response = requests.post(
+                        url, json={"arg": order.order_id}, verify=True
+                    )
 
-                if (
-                    response.status_code == 200
-                    and response.json().get("status") == "ok"
-                ):
-                    order.status = FailedOrder.COMPLETED
-                    order.message = "Procesado con éxito."
-                    order.save()
+                    if (
+                        response.status_code == 200
+                        and response.json().get("status") == "ok"
+                    ):
+                        order.status = FailedOrder.COMPLETED
+                        order.message = "Procesado con éxito."
+                        order.save()
+
+                except Exception as e:
+                    current_time = datetime.now().strftime("%d/%m/%Y %H:%M")
+                    self.stdout.write(
+                        self.style.ERROR(
+                            f"{current_time} | Error processing order {order.order_id}: {e}"
+                        )
+                    )
+                    continue
 
         except Exception as e:
+            current_time = datetime.now().strftime("%d/%m/%Y %H:%M")
             self.stdout.write(
-                self.style.ERROR("An error occurred while processing failed orders.")
+                self.style.ERROR(
+                    f"{current_time} | An error occurred while processing failed orders."
+                )
             )
             self.stdout.write(str(e))
