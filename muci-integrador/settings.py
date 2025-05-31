50c5d457377acdb03bb497bc014fccf8a1af7e0e
Carlos Vallory <carlvallory@gmail.com>
2025-05-31 17:03:55 -0300

    logging enable

--- muci-integrador/settings.py
+++ muci-integrador/settings.py
@@ -96,6 +96,20 @@ DATABASES = {
     }
 }
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+}
+
 
 LANGUAGE_CODE = "es-PY"
 
