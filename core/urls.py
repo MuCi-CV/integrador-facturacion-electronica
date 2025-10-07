50965ceb450840f6e633e76cee66e9f8b480bb2c
Carlos Vallory <carlvallory@gmail.com>
2025-10-07 06:08:04 -0300

    Mod add Forgot Pwd 1.1

--- core/urls.py
+++ core/urls.py
@@ -1,35 +1,8 @@
 from django.urls import path, re_path, include
-from django.contrib.auth import views as auth_views
 
 from core.views import SalesView, RefundView
 
 urlpatterns = [
     path("sales/", SalesView.as_view()),
     path("refunds/", RefundView.as_view()),
-    
-    path('forgot-password/', 
-         auth_views.PasswordResetView.as_view(
-             template_name='registration/password_reset_form.html',
-             email_template_name='registration/password_reset_email.html',
-             subject_template_name='registration/password_reset_subject.txt'
-         ), 
-         name='password_reset'),
-    
-    path('forgot-password/done/', 
-         auth_views.PasswordResetDoneView.as_view(
-             template_name='registration/password_reset_done.html'
-         ), 
-         name='password_reset_done'),
-    
-    path('reset/<uidb64>/<token>/', 
-         auth_views.PasswordResetConfirmView.as_view(
-             template_name='registration/password_reset_confirm.html'
-         ), 
-         name='password_reset_confirm'),
-    
-    path('reset/done/', 
-         auth_views.PasswordResetCompleteView.as_view(
-             template_name='registration/password_reset_complete.html'
-         ), 
-         name='password_reset_complete'),
 ]
